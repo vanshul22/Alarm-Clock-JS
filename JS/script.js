9640e5fa9320955288dbb1907902765c812b66c7
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-02 21:22:02 +0530

    Version : 1.1.1 initial Commit.

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+/*
+Website Name - Alarm-Clock
+Name : Vanshul Kesharwani
+Date : 02/04/2022
+Version : 1.1.1
+Email : vkvanshulkesharwani54@gmail.com
+Description : This is a Web app for adding alarm clock.
+*/
---
+++ README.md
@@ -0,0 +1,4 @@
+# Alarm-Clock
+This is a Web application for Alarm Clock.
+
+To check live page please [Click Here](https://vanshul22.github.io/Alarm-Clock/).
