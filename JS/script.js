1deb705b96a36556713d189594c7b779b981fe14
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-04-04 12:56:12 +0530

    Version 3.1.1 : Display current time in Alarm Clock.

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 Website Name - Alarm-Clock
 Name : Vanshul Kesharwani
 Date : 02/04/2022
-Version : 2.1.1
+Version : 3.1.1
 Email : vkvanshulkesharwani54@gmail.com
 Description : This is a Web app for adding alarm clock.
 */
@@ -40,4 +40,34 @@ function ringAlarm() {
     } else {
         console.log("Date & time is not correct.");
     };
+};
+
+// Added Current time from here.
+
+function updateClock() {
+    // Taking current time here
+    let currentTime = new Date();
+
+    // Taking all hour, minute and second in different2 variables.
+    let currentHours = currentTime.getHours();
+    let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
+
+    // Fixing Minutes and hours and seconds if time is less than 10 then we will add 0 in front of it.
+    currentHours = (currentHours < 10 ? "0" : "") + currentHours;
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+    // For AM/PM 
+    let timeOfDay = (currentHours < 12) ? "AM" : "PM";
+
+    // Fixing time from railway to 12 hour format.
+    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+    currentHours = (currentHours == 0) ? 12 : currentHours;
+
+    // Creating string of current time by 3 vars.
+    let currentTimeStr = `${currentHours}:${currentMinutes}:${currentSeconds} ${timeOfDay}`;
+
+    // Showing current time in DOM.
+    document.getElementById("currentTimeShow").innerHTML = currentTimeStr;
 };
